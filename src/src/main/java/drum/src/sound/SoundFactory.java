db73e57e3c87d36f60aa7c022343f843088520c9
Sena Deniz Avukat <senavukat@gmail.com>
2024-03-29 11:22:58 +0100

    Command Design Pattern

--- src/src/main/java/drum/src/sound/SoundFactory.java
+++ src/src/main/java/drum/src/sound/SoundFactory.java
@@ -3,10 +3,22 @@ import drum.src.ui.SoundButton;
 
 import java.util.*;
 
+/**
+ * The SoundFactory class manages the creation and retrieval of Sound objects.
+ * It utilizes the Flyweight pattern to  reuse and manage existing Sound instances
+ * and reduce memory overhead.
+ */
 public class SoundFactory {
 
+    /**
+     * Map to hold Sound objects
+     * Conventions:
+     * String :The name of the new sound.
+     * Sound : uses constructor with (SoundName, PAth from Repository Root )
+     */
     private static final Map<String, Sound> sounds = new HashMap<>();
 
+    // Static initializer to preload sounds
     static {
         loadSounds();
     }
@@ -20,15 +32,12 @@ public class SoundFactory {
     }
 
 
-   public static Sound getSound(String soundName) {
-       return sounds.get(soundName);
-   }
+    public static Sound getSound(String soundName) {
+        return sounds.get(soundName);
+    }
 
     public static void addSound(String soundName, String soundFile) {
         sounds.putIfAbsent(soundName, new Sound(soundName, soundFile));
-    }
-    public static Iterator<String> getSoundNamesIterator() {
-        return sounds.keySet().iterator();
     }
     public static Set<String> getAllSoundNames() {
         return Collections.unmodifiableSet(sounds.keySet());
